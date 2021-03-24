87c2cf40e2997c3c174d4e0a3c831f130595dfc6
Ananya Chandramohan <77439415+AnanyaC97@users.noreply.github.com>
2021-03-24 17:34:11 +0530

    Update SensorValidatorTest.cs

--- SensorValidate.Tests/SensorValidatorTest.cs
+++ SensorValidate.Tests/SensorValidatorTest.cs
@@ -9,25 +9,25 @@ namespace SensorValidate.Tests
     {
         [Fact]
         public void reportsErrorWhenSOCjumps() {
-            Assert.False(SensorValidator.ValidateReadings(
+            Assert.False(SensorValidator.CheckSensorReadings(
                 new List<double>{0.0, 0.01, 0.5, 0.51}, 0.05
             ));
         }
         [Fact]
         public void reportsErrorWhenCurrentjumps() {
-            Assert.False(SensorValidator.ValidateReadings(
+            Assert.False(SensorValidator.CheckSensorReadings(
                 new List<double>{0.03, 0.03, 0.03, 0.33}, 0.1
             ));
         }
         [Fact]
         public void reportsErrorOnEmptySOCReadings() {
-            Assert.False(SensorValidator.ValidateReadings(
+            Assert.False(SensorValidator.CheckSensorReadings(
                 new List<double>{}, 0.05
             ));
         }
         [Fact]
         public void reportsErrorOnEmptyCurrentReadings() {
-            Assert.False(SensorValidator.ValidateReadings(
+            Assert.False(SensorValidator.CheckSensorReadings(
                 new List<double>{}, 0.1
             ));
         }
