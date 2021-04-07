7e239229ba7eaeff9117182c19125c1cd4f92fae
Ananya Chandramohan <77439415+AnanyaC97@users.noreply.github.com>
2021-04-07 09:53:37 +0530

    Update SensorValidatorTest.cs

--- SensorValidate.Tests/SensorValidatorTest.cs
+++ SensorValidate.Tests/SensorValidatorTest.cs
@@ -8,27 +8,31 @@ namespace SensorValidate.Tests
     public class SensorValidatorTest
     {
         [Fact]
-        public void reportsErrorWhenSOCjumps() {
-            Assert.False(SensorValidator.CheckSensorReadings(
-                new List<double>{0.0, 0.01, 0.5, 0.51}, 0.05
+        public void reportsErrorWhenSOCjumps()
+        {
+            Assert.False(SensorValidator.CheckSensorMeasureReadings(
+                new List<double> { 0.0, 0.01, 0.5, 0.51 }, 0.05
             ));
         }
         [Fact]
-        public void reportsErrorWhenCurrentjumps() {
-            Assert.False(SensorValidator.CheckSensorReadings(
-                new List<double>{0.03, 0.03, 0.03, 0.33}, 0.1
+        public void reportsErrorWhenCurrentjumps()
+        {
+            Assert.False(SensorValidator.CheckSensorMeasureReadings(
+                new List<double> { 0.03, 0.03, 0.03, 0.33 }, 0.1
             ));
         }
         [Fact]
-        public void reportsErrorOnEmptySOCReadings() {
-            Assert.False(SensorValidator.CheckSensorReadings(
-                new List<double>{}, 0.05
+        public void reportsErrorOnEmptySOCReadings()
+        {
+            Assert.False(SensorValidator.CheckSensorMeasureReadings(
+                new List<double> { }, 0.05
             ));
         }
         [Fact]
-        public void reportsErrorOnEmptyCurrentReadings() {
-            Assert.False(SensorValidator.CheckSensorReadings(
-                new List<double>{}, 0.1
+        public void reportsErrorOnEmptyCurrentReadings()
+        {
+            Assert.False(SensorValidator.CheckSensorMeasureReadings(
+                new List<double> { }, 0.1
             ));
         }
     }
