2f4fbd687c81eb490e1f15e626e5c01f24000941
Ananya Chandramohan <77439415+AnanyaC97@users.noreply.github.com>
2021-04-07 09:52:59 +0530

    Update SensorValidate.cs

--- SensorValidate/SensorValidate.cs
+++ SensorValidate/SensorValidate.cs
@@ -1,34 +1,34 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 namespace SensorValidate
 {
     public class SensorValidator
     {
-        public static bool CheckForSuddenValueJump(double value, double nextValue, double maxDelta) {
-            if(nextValue - value > maxDelta) {
-                return false;
+        public static bool CheckForSuddenValueJump(double value, double nextValue, double maxDelta)
+        {
+            if (nextValue - value > maxDelta) {
+                return true;
             }
-            return true;
+            return false;
         }
-        public static bool ValidateSensorReadings(List<Double> values, double maxReading)
+        public static bool ValidateSensorMeasureReadings(List<Double> values, double maxReading)
         {
             int lastButOneIndex = values.Count - 1;
             for (int i = 0; i < lastButOneIndex; i++)
             {
-                if (!CheckForSuddenValueJump(values[i], values[i + 1], maxReading))
-                {
+                if (CheckForSuddenValueJump(values[i], values[i + 1], maxReading)) {
                     return false;
                 }
             }
             return true;
         }
-        public static bool CheckSensorReadings(List<Double> values, double maxReading)
+        public static bool CheckSensorMeasureReadings(List<Double> values, double maxReading)
         {
-            if (values.Count == 0)
+            if (values == null)
                 return false;
             else
-                return ValidateSensorReadings(values, maxReading);
+                return ValidateSensorMeasureReadings(values, maxReading);
         }
     }
 }
